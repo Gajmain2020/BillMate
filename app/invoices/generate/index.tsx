591f29802d5f68af546ab962145fac1c007a4ce1
Gajendra Sahu <gajmain2020@gmail.com>
2025-01-14 23:02:59 +0530

    feat: custom form input complete

--- app/invoices/generate/index.tsx
+++ app/invoices/generate/index.tsx
@@ -1,19 +1,35 @@
-import { Text } from 'react-native';
+import { FormProvider, useForm } from 'react-hook-form';
+import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { Button } from '~/components/Button';
 import CustomTextInput from '~/components/CustomTextInput';
 
 export default function GenerateInvoice() {
+  const form = useForm();
+
+  const onSubmit = (data: any) => {
+    console.warn('all good go to next step');
+  };
+
   return (
     <SafeAreaView edges={['bottom']} className="flex-1 p-4">
-      <Text className="mb-5 text-2xl font-bold">Sender Info</Text>
+      <FormProvider {...form}>
+        <Text className="mb-5 text-2xl font-bold">Sender Info</Text>
 
-      <CustomTextInput label="Name" placeholder="Enter your name" />
-      <CustomTextInput label="Address" placeholder="Enter your address" multiline />
-      <CustomTextInput label="Tax ID" placeholder="Enter your tax id" />
+        <View className="gap-2">
+          <CustomTextInput name="name" label="Name" placeholder="Enter your name" />
+          <CustomTextInput
+            name="address"
+            label="Address"
+            placeholder="Enter your address"
+            multiline
+          />
+          <CustomTextInput name="texId" label="Tax ID" placeholder="Enter your tax id" />
+        </View>
 
-      <Button title="Next" className="mt-auto" />
+        <Button title="Next" className="mt-auto" onPress={form.handleSubmit(onSubmit)} />
+      </FormProvider>
     </SafeAreaView>
   );
 }
--- components/CustomTextInput.tsx
+++ components/CustomTextInput.tsx
@@ -1,15 +1,28 @@
+import { Control, useController } from 'react-hook-form';
 import { Text, TextInput, TextInputProps, View } from 'react-native';
 
 type CustomTextInputProps = {
+  name: string;
   label: string;
 } & TextInputProps;
 
-export default function CustomTextInput({ label, ...props }: CustomTextInputProps) {
+export default function CustomTextInput({ label, name, ...props }: CustomTextInputProps) {
+  const {
+    field: { onChange, onBlur, value },
+    fieldState: { error },
+  } = useController({ name, rules: { required: 'Field is required.' } });
   return (
-    <View className="mb-4">
+    <View className="gap-2">
       <Text>{label}</Text>
 
-      <TextInput {...props} className={`rounded border border-gray-200 p-4 ${props.className}`} />
+      <TextInput
+        {...props}
+        className={`rounded border border-gray-200 p-4 ${props.className}`}
+        value={value}
+        onChangeText={onChange}
+        onBlur={onBlur}
+      />
+      <Text className="text-red-500">{error?.message}</Text>
     </View>
   );
 }
