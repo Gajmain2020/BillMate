150b7222bad4610d2540683046f29f14b42d84f0
Gajendra Sahu <gajmain2020@gmail.com>
2025-01-15 12:00:52 +0530

    chore: added the header title for generate

--- app/invoices/generate/_layout.tsx
+++ app/invoices/generate/_layout.tsx
@@ -1,5 +1,13 @@
 import { Stack } from 'expo-router';
 
 export default function GenerateInvoiceLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: 'Sender Information' }} />
+      <Stack.Screen name="invoice-info" options={{ title: 'Invoice Information' }} />
+      <Stack.Screen name="recipient" options={{ title: 'Recipient Information' }} />
+      <Stack.Screen name="items" options={{ title: 'Items' }} />
+      <Stack.Screen name="summary" options={{ title: 'Summary' }} />
+    </Stack>
+  );
 }
