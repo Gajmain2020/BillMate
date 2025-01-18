88361d56e0d9a94765d362462bcf544e0badf126
Gajendra Sahu <gajmain2020@gmail.com>
2025-01-18 12:06:53 +0530

    feat: persistent storage added

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,20 @@
-import { Stack, Link } from 'expo-router';
+import { Stack, Link, router } from 'expo-router';
 import { ImageBackground, Text, View } from 'react-native';
 
 import { Button } from '~/components/Button';
 import KeyboardAwareScrollView from '~/components/KeyboardAwareScrollView';
+import { useStore } from '~/store';
 
 export default function Home() {
+  const existingNewInvoice = useStore((data) => data.newInvoice);
+
+  const onNewInvoice = () => {
+    router.push('/invoices/generate');
+  };
+
   return (
     <KeyboardAwareScrollView>
-      <Stack.Screen options={{ title: 'Home', headerShown: false }} />
+      <Stack.Screen options={{ headerShown: false }} />
       <View className="flex-1 justify-center gap-8 ">
         <ImageBackground
           className="absolute h-full w-full opacity-10"
@@ -20,9 +27,10 @@ export default function Home() {
             Create and share professional invoices in seconds
           </Text>
         </View>
-        <Link href={{ pathname: '/invoices/generate' }} asChild>
-          <Button title="New Invoice" />
-        </Link>
+        <Button
+          onPress={onNewInvoice}
+          title={existingNewInvoice ? 'Resume Invoicing' : 'New Invoice'}
+        />
       </View>
     </KeyboardAwareScrollView>
   );
--- store/index.ts
+++ store/index.ts
@@ -1,4 +1,6 @@
+import Storage from 'expo-sqlite/kv-store';
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 import { Invoice, BusinessEntityType, InvoiceInfoType, InvoiceItemType } from '~/schema/invoice';
 
@@ -6,8 +8,10 @@ import { Invoice, BusinessEntityType, InvoiceInfoType, InvoiceItemType } from '~
 
 export type InvoiceState = {
   newInvoice: Partial<Invoice> | null;
+
   startNewInvoice: () => void;
   resetNewInvoice: () => void;
+
   addSenderInfo: (sender: BusinessEntityType) => void;
   addRecipientInfo: (recipient: BusinessEntityType) => void;
   addInvoiceInfo: (invoiceInfo: InvoiceInfoType) => void;
@@ -17,35 +21,40 @@ export type InvoiceState = {
   getTotal: () => number;
 };
 
-export const useStore = create<InvoiceState>((set, get) => ({
-  newInvoice: null,
-  startNewInvoice: () =>
-    set(() => ({
-      newInvoice: {
-        items: [{ name: 'Example', quantity: 1, price: 20 }],
-        date: new Date(),
-        // dueDate: new Date(new Date().setDate(new Date().getDate() + 14)).toLocaleDateString(),
+export const useStore = create<InvoiceState>()(
+  persist(
+    (set, get) => ({
+      newInvoice: null,
+      startNewInvoice: () =>
+        set(() => ({
+          newInvoice: {
+            items: [{ name: 'Example', quantity: 1, price: 20 }],
+            date: new Date(),
+            // dueDate: new Date(new Date().setDate(new Date().getDate() + 14)).toLocaleDateString(),
+          },
+        })),
+      resetNewInvoice: () => set(() => ({ newInvoice: null })),
+      addSenderInfo: (sender) => set((state) => ({ newInvoice: { ...state.newInvoice, sender } })),
+      addRecipientInfo: (recipient) =>
+        set((state) => ({ newInvoice: { ...state.newInvoice, recipient } })),
+      addInvoiceInfo: (invoiceInfo) =>
+        set((state) => ({ newInvoice: { ...state.newInvoice, ...invoiceInfo } })),
+      addItems: (items) => set((state) => ({ newInvoice: { ...state.newInvoice, items } })), //todo: may be we should append items
+      getSubtotal: () => {
+        const items = get().newInvoice?.items || [];
+        return items.reduce((acc, item) => acc + item.price * item.quantity, 0);
       },
-    })),
-  resetNewInvoice: () => set(() => ({ newInvoice: null })),
-  addSenderInfo: (sender) => set((state) => ({ newInvoice: { ...state.newInvoice, sender } })),
-  addRecipientInfo: (recipient) =>
-    set((state) => ({ newInvoice: { ...state.newInvoice, recipient } })),
-  addInvoiceInfo: (invoiceInfo) =>
-    set((state) => ({ newInvoice: { ...state.newInvoice, ...invoiceInfo } })),
-  addItems: (items) => set((state) => ({ newInvoice: { ...state.newInvoice, items } })), //todo: may be we should append items
-  getSubtotal: () => {
-    const items = get().newInvoice?.items || [];
-    return items.reduce((acc, item) => acc + item.price * item.quantity, 0);
-  },
-  getGst: () => {
-    const items = get().newInvoice?.items || [];
-    const subtotal = items.reduce((acc, item) => acc + item.price * item.quantity, 0);
-    return subtotal * 0.05;
-  },
-  getTotal: () => {
-    const items = get().newInvoice?.items || [];
-    const subtotal = items.reduce((acc, item) => acc + item.price * item.quantity, 0);
-    return subtotal + subtotal * 0.05;
-  },
-}));
+      getGst: () => {
+        const items = get().newInvoice?.items || [];
+        const subtotal = items.reduce((acc, item) => acc + item.price * item.quantity, 0);
+        return subtotal * 0.05;
+      },
+      getTotal: () => {
+        const items = get().newInvoice?.items || [];
+        const subtotal = items.reduce((acc, item) => acc + item.price * item.quantity, 0);
+        return subtotal + subtotal * 0.05;
+      },
+    }),
+    { name: 'billmate-store', getStorage: () => Storage }
+  )
+);
